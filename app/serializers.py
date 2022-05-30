8e3415e561f74ee381ac453fe4b6209159b5357b
Mrudhulraj <mrudhulraj.n@gmail.com>
2022-05-30 15:01:03 +0530

    ModelSerializer

--- app/serializers.py
+++ app/serializers.py
@@ -2,11 +2,10 @@ from rest_framework import serializers
 from .models import Employee
 
 
-class EmployeeSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=30)
-    email = serializers.EmailField()
-    password = serializers.CharField(max_length=30)
-    phone = serializers.CharField(max_length=10)
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
 
     def create(self, validated_data):
         return Employee.objects.create(**validated_data)
